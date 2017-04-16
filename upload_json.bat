90a584308bdf3dccae8dd641088bc9fdb0fa66b0
Andreas J <andreas@jungierek.de>
2017-04-16 09:25:51 +0200

    upload scripts are now line by line

--- upload_json.bat
+++ upload_json.bat
@@ -1,1 +1,24 @@
-nodemcu-tool.cmd upload espConfig_982283.json espConfig_13423040.json espConfig_13423304.json espConfig_1461824.json espConfig_15892791.json espConfig_16061971.json espConfig_1689710.json espConfig_1829768.json espConfig_2028701.json espConfig_2030164.json espConfig_2677473.json espConfig_35778.json espConfig_485535.json espConfig_518010.json espConfig_8391351.json espConfig_8578563.json espConfig_13882703.json espConfig_10334652.json espConfig_15317270.json espConfig_15317277.json espConfig_default.json
+nodemcu-tool.cmd upload ^
+espConfig_default.json ^
+espConfig_35778.json ^
+espConfig_485535.json ^
+espConfig_518010.json ^
+espConfig_982283.json ^
+espConfig_1461824.json ^
+espConfig_1689710.json ^
+espConfig_1829768.json ^
+espConfig_2028701.json ^
+espConfig_2030164.json ^
+espConfig_2677460.json ^
+espConfig_2677473.json ^
+espConfig_8391351.json ^
+espConfig_8578563.json ^
+espConfig_10334652.json ^
+espConfig_13371818.json ^
+espConfig_13423040.json ^
+espConfig_13423304.json ^
+espConfig_13882703.json ^
+espConfig_15317270.json ^
+espConfig_15317277.json ^
+espConfig_15892791.json ^
+espConfig_16061971.json
--- upload_lua.bat
+++ upload_lua.bat
@@ -1,1 +1,18 @@
-nodemcu-tool.cmd upload buttonNode.lua espConfig.lua garageNode.lua httpDL.lua mqttNode.lua noNode.lua relayNode.lua rfNode.lua sonoffNode.lua startup.lua tempNode.lua update.lua util.lua xmasNode.lua _init.lua
+nodemcu-tool.cmd upload ^
+buttonNode.lua ^
+credential.lua ^
+espConfig.lua ^
+garageNode.lua ^
+httpDL.lua ^
+ledNode.lua ^
+mqttNode.lua ^
+noNode.lua ^
+relayNode.lua ^
+rfNode.lua ^
+sonoffNode.lua ^
+startup.lua ^
+tempNode.lua ^
+update.lua ^
+util.lua ^
+xmasNode.lua ^
+_init.lua
