3a381c71fa845510bf2a0001714df3bed6e321c7
Andreas J <andreas@jungierek.de>
2017-07-02 11:58:49 +0200

    V0.40

--- espConfig.lua
+++ espConfig.lua
@@ -14,7 +14,7 @@ _G [moduleName] = M;
 --------------------------------------------------------------------
 -- vars
 
-local VERSION = "V0.39"
+local VERSION = "V0.40"
 
 local PROD_GATEWAY = "192.168.2.1";
 local PROD_NETMASK = "255.255.255.0";
