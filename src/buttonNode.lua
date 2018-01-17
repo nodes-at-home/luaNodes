c3c260f10e437af15ef184b04e802d16b9f6cc15
Andreas J <andreas@jungierek.de>
2018-01-17 22:29:28 +0100

    button node changed to using deepsleep module
    - usage of flag useOfflineCallback removed
    - deepsleep timer id deleted

--- src/buttonNode.lua
+++ src/buttonNode.lua
@@ -14,6 +14,11 @@ _G [moduleName] = M;
 -------------------------------------------------------------------------------
 --  Settings
 
+local offDelay = nodeConfig.timer.offDelay or 3000;
+local deepSleepDelay = nodeConfig.timer.deepSleepDelay;
+
+local retain = 0; -- NO retain!!!
+
 ----------------------------------------------------------------------------------------
 -- private
 
@@ -23,34 +28,6 @@ local restartConnection = true;
 -- public
 -- mqtt callbacks
 
-local function goDeepSleep ( client, baseTopic )
-
-    if ( not useOfflineCallback ) then
-        restartConnection = false;
-        print ( "[APP] initiate alarm for closing connection in " ..  nodeConfig.timer.deepSleepDelay/1000 .. " seconds" );
-        -- wait a minute with closing connection
-        tmr.alarm ( nodeConfig.timer.deepSleep, nodeConfig.timer.deepSleepDelay, tmr.ALARM_SINGLE,  -- timer_id, interval_ms, mode
-            function () 
-                -- publishing OFF is not harmful
-                print ( "[APP] publish button press OFF" );
-                client:publish ( baseTopic .. "/value/state", "OFF", 0, 0, -- qos, NO retain!!!
-                    function ( client )
-                        print ( "[APP] closing connection" );
-                        client:close ();
-                        print ( "[APP] Going to deep sleep forever" );
-                        node.dsleep ( 0, 1 ); -- sleep forever, with RF_CAL
-                    end
-                );
-            end
-        );
-    else
-        print ( "[APP] closing connection using offline handler" );
-        -- TODO falls das wieder benutzt wird, dann hier noch publish etc. einbauen
-        client:close ();
-    end
-
-end
-
 function M.connect ( client, baseTopic )
 
     print ( "[APP] connect" );
@@ -58,26 +35,25 @@ function M.connect ( client, baseTopic )
     local rawcode, bootreason = node.bootreason ();
     if ( bootreason == 5 ) then -- 5 = wake from deep sleep
         print ( "[APP] publish button press ON" );
-        client:publish ( baseTopic .. "/value/state", "ON", 0, 0, -- qos, NO retain!!!
+        client:publish ( baseTopic .. "/value/state", "ON", 0, retain, -- qos, NO retain!!!
             function ( client )
-                goDeepSleep ( client, baseTopic );
+                tmr.create ():alarm ( offDelay, tmr.ALARM_SINGLE,  -- timer_id, interval_ms, mode
+                    function () 
+                        -- publishing OFF is not harmful
+                        print ( "[APP] publish button press OFF" );
+                        client:publish ( baseTopic .. "/value/state", "OFF", 0, retain, -- qos, NO retain!!!
+                            function ( client )
+                                require ( "deepsleep").go ( client, deepSleepDelay, 0 ); -- sleep forever
+                            end
+                        );
+                    end
+                );
             end
         );
     else
-        goDeepSleep( client, baseTopic );
+        require ( "deepsleep").go ( client, deepSleepDelay, 0 ); -- sleep forever
     end
 
-end
-
-local function offline ( client )
-
-    print ( "[APP] offline" );
-
-    print ( "[APP] Going to deep sleep forever" );
-    node.dsleep ( 0, 1 ); -- sleep forever, with RF_CAL
-    
-    return restartConnection; -- dont restart mqtt connection
-    
 end
 
 function M.offline ( client )
@@ -98,10 +74,6 @@ end
 -- main
 
 print ( "[MODULE] loaded: " .. moduleName )
-
-if ( nodeConfig.appCfg.useOfflineCallback ) then
-    M.offline = offline;
-end
 
 return M;
 
