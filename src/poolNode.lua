faee32d570e4518499c0f52c42b07312b7d63dca
Andreas J <andreas@jungierek.de>
2018-01-17 22:31:55 +0100

    pool node changed to using deepsleep module
    - usage of flag useOfflineCallback removed
    - deepsleep timer id deleted

--- src/poolNode.lua
+++ src/poolNode.lua
@@ -14,59 +14,20 @@ _G [moduleName] = M;
 -------------------------------------------------------------------------------
 --  Settings
 
-----------------------------------------------------------------------------------------
--- private
-
 local dsPin = nodeConfig.appCfg.dsPin or 3;
 local restartConnection = true;
+
+local deepSleepDelay = nodeConfig.timer.deepSleepDelay;
+local timeBetweenSensorReadings = nodeConfig.appCfg.timeBetweenSensorReadings;
+
+local retain = nodeConfig.mqtt.retain;
+
+----------------------------------------------------------------------------------------
+-- private
 
 --------------------------------------------------------------------
 -- public
 -- mqtt callbacks
-
-local function goDeepSleep ( client )
-
-    if ( not nodeConfig.appCfg.useOfflineCallback ) then
-        restartConnection = false;
-        local deepSleepDelay = nodeConfig.timer.deepSleepDelay;
-        print ( "[APP] initiate alarm for closing connection in " ..  deepSleepDelay/1000 .. " seconds" );
-        -- wait a minute with closing connection
-        tmr.alarm ( nodeConfig.timer.deepSleep, deepSleepDelay, tmr.ALARM_SINGLE,  -- timer_id, interval_ms, mode
-            function () 
-                print ( "[APP] closing connection" );
-                client:close ();
-                local timeBetweenSensorReadings = nodeConfig.appCfg.timeBetweenSensorReadings;
-                print ( "[APP] Going to deep sleep for ".. timeBetweenSensorReadings/1000 .." seconds" );
-                node.dsleep ( (timeBetweenSensorReadings - deepSleepDelay) * 1000, 1 ); -- us, RF_CAL after deep sleep
-            end
-        );
-    else
-        print ( "[APP] closing connection using offline handler" );
-        client:close ();
-    end
-
-end
-
-local function publishValues ( client, baseTopic, temperature )
-
-    if ( temperature ) then
-        print ( "[APP] publish temperature t=" .. temperature );
-        client:publish ( baseTopic .. "/value/temperature", [[{"value":]] .. temperature .. [[,"unit":"°C"}]], 0, nodeConfig.retain, -- qos, retain
-            function ( client )
-                goDeepSleep ( client );
-            end
-        );
-    else
-        print ( "[APP] nothing published" );
-        local t = temperature and temperature or "--";
-        client:publish ( baseTopic .. "/value/error", "nothing published t=" .. t, 0, nodeConfig.retain, -- qos, retain
-            function ( client )
-                goDeepSleep ( client );
-            end
-        );
-    end
-
-end
 
 function M.start ( client, topic)
 
@@ -82,27 +43,19 @@ function M.connect ( client, topic )
     
     ds18b20.read (
         function ( index, address, resolution, temperature, tempinteger, parasitic )
-            -- only first sensor
-            if ( index == 1 ) then
+            if ( index == 1 ) then -- only first sensor
                 local addr = string.format ( "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", string.match ( address, "(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+):(%d+)" ) );
                 print ( "[APP] index=" .. index .. " address=" .. addr .. " resolution=" .. resolution .. " temperature=" .. temperature .. " parasitic=" .. parasitic );
-                publishValues ( client, topic, temperature );
+                print ( "[APP] publish temperature t=" .. temperature );
+                client:publish ( topic .. "/value/temperature", [[{"value":]] .. temperature .. [[,"unit":"°C"}]], 0, retain, -- qos, retain
+                    function ( client )
+                        require ( "deepsleep" ).go ( client, deepSleepDelay, timeBetweenSensorReadings );
+                    end
+                );
             end
         end,
         {}
     );
-    
-end
-
-local function offline ( client )
-
-    print ( "[APP] offline" );
-
-    local timeBetweenSensorReadings = nodeConfig.appCfg.timeBetweenSensorReadings;
-    print ( "[APP] Going to deep sleep for ".. timeBetweenSensorReadings/1000 .." seconds" );
-    node.dsleep ( timeBetweenSensorReadings * 1000, 1 ); -- us, RF_CAL after deep sleep
-    
-    return restartConnection; -- restart mqtt connection
     
 end
 
@@ -124,10 +77,6 @@ end
 -- main
 
 print ( "[MODULE] loaded: " .. moduleName )
-
-if ( nodeConfig.appCfg.useOfflineCallback ) then
-    M.offline = offline;
-end
 
 return M;
 
