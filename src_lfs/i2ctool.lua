3882ecf5270569b6498e7577982413e0fd70025c
Andreas <andreas@jungierek.de>
2024-08-13 22:26:56 +0200

    i2c fixings and refactorings

--- src_lfs/i2ctool.lua
+++ src_lfs/i2ctool.lua
@@ -20,14 +20,16 @@ local i2c, bit = i2c, bit;
 
 local ID = 0;
 
-local deviceAdress; -- i2c address: first 7 bits are the device address, last bit marks read/write mode, coding is done by i2c.adderss
+local deviceAdress; -- i2c address: first 7 bits are the device address, last bit marks read/write mode, coding is done by i2c.address
 
 -------------------------------------------------------------------------------
 -- i2c basics
 
 function M.readByte ( register )
 
-    logger:debug ( "readByte: addr=" .. tohex ( deviceAdress )  .. " reg=" .. tohex ( register ) )
+    assert ( register, "register is undefined" );
+
+    logger:debug ( "readByte: addr=" .. tohex ( deviceAdress )  .. " register=" .. tohex ( register ) );
 
     return string.byte ( M.readBytes ( register, 1 ), 1 );
 
@@ -35,19 +37,23 @@ end
 
 function M.readBytes ( register, len )
 
-    logger:debug ( "readBytes: addr=" .. tohex ( deviceAdress ) .. " reg=" .. tohex ( register ) .. " len=" .. len )
+    assert ( register, "register is undefined" );
+    assert ( len, "len is undefined" );
+
+    logger:debug ( "readBytes: addr=" .. tohex ( deviceAdress ) .. " register=" .. tohex ( register ) .. " len=" .. len );
 
     i2c.start ( ID );
     local ackTransmit = i2c.address ( ID, deviceAdress, i2c.TRANSMITTER );
-    logger:debug ( "readBytes: ack transmit=" .. tostring ( ackTransmit ) .. " addr=" .. tohex ( deviceAdress ) );
+    logger:debug ( "readBytes: ack transmit=" .. tostring ( ackTransmit ) );
     local n = i2c.write ( ID, register );
-    logger:debug ( "readBytes: n=" .. n )
-    i2c.stop ( ID );
+    logger:debug ( "readBytes: n=" .. n );
+    --i2c.stop ( ID );
 
     i2c.start ( ID );
     local ackReceive = i2c.address ( ID, deviceAdress, i2c.RECEIVER );
     logger:debug ( "readBytes: ack receive=" .. tostring ( ackReceive ) )
     local data = i2c.read ( ID, len );
+    logger:debug ( "readBytes: #data=" .. string.len ( data ) .. " data=" .. table.concat ( { string.byte ( data, 1, #data ) }, ", " ) );
     i2c.stop ( ID );
 
     return data;
@@ -56,41 +62,67 @@ end
 
 function M.writeByte ( register, byte )
 
-    logger:debug ( "writeByte: addr=" .. tohex ( deviceAdress )  .. " reg=" .. tohex ( register ) .. " byte=" .. tohex ( byte ) )
+    assert ( register, "register is undefined" );
+    assert ( byte, "byte is undefined" );
+
+    logger:debug ( "writeByte: addr=" .. tohex ( deviceAdress )  .. " register=" .. tohex ( register ) .. " byte=" .. tohex ( byte ) )
 
     i2c.start ( ID );
     local ackTransmit = i2c.address ( ID, deviceAdress, i2c.TRANSMITTER );
-    logger:debug ( "writeByte: ack transmit=" .. tostring ( ackTransmit ) )
+    logger:debug ( "writeByte: ack transmit=" .. tostring ( ackTransmit ) );
     local n1 = i2c.write ( ID, register );
-    logger:debug ( "writeByte: n1=" .. n1 )
+    logger:debug ( "writeByte: n1=" .. n1 );
     local n2 = i2c.write ( ID, byte );
-    logger:debug ( "writeByte: n2=" .. n2 )
+    logger:debug ( "writeByte: n2=" .. n2 );
     i2c.stop ( ID );
 
 end
 
-function M.setBit ( reg, pos, value )
+function M.writeBytes ( register, bytes )
 
-    --logger:debug ( "setBit: reg=" .. tohex ( register ) .. " pos=" .. pos .. " value=" .. tohex ( value ) )
+    assert ( register, "register is undefined" );
+    assert ( bytes, "bytes is undefined" );
+    assert ( type ( bytes ) == "table", "bytes is not a table" );
+
+    logger:debug ( "writeBytes: addr=" .. tohex ( deviceAdress )  .. " register=" .. tohex ( register ) .. " bytes=" .. table.concat ( bytes, ", " ) );
+
+    i2c.start ( ID );
+    local ackTransmit = i2c.address ( ID, deviceAdress, i2c.TRANSMITTER );
+    logger:debug ( "writeBytes: ack transmit=" .. tostring ( ackTransmit ) );
+    local n1 = i2c.write ( ID, register );
+    logger:debug ( "writeBytes: n1=" .. n1 )
+    local n2 = i2c.write ( ID, unpack ( bytes ) );
+    logger:debug ( "writeBytes: n2=" .. n2 );
+    i2c.stop ( ID );
+
+end
+
+function M.setBit ( register, pos, value )
+
+    assert ( register, "register is undefined" );
+    assert ( pos, "pos is undefined" );
+    assert ( value, "value is undefined" );
+
+    --logger:debug ( "setBit: register=" .. tohex ( register ) .. " pos=" .. pos .. " value=" .. tohex ( value ) )
 
     if ( value == nil ) then value = 1; end
     local handleBit = value and bit.set or bit.clear;
-    local n = M.writeByte ( reg, handleBit ( M.readByte ( reg ), pos ) );
+    local n = M.writeByte ( register, handleBit ( M.readByte ( register ), pos ) );
     --logger:debug ( "setBit: n=" .. n )
 
 end
 
-function M.setBits ( reg, highest, lowest, value )
+function M.setBits ( register, highest, lowest, value )
 
---    assert ( reg, "reg is undefined" );
+--    assert ( register, "register is undefined" );
 --    assert ( value, "value is undefined" );
 --    assert ( 8 >= highest and highest >= 0, "highest bit is outside (highest=" .. highest .. ")" );
 --    assert ( 8 >= lowest and lowest >= 0, "lowest bit is outside (lowest=" .. lowest .. ")" );
 --    assert ( highest >= lowest, "wrong order (highest=" .. highest .. ", lowest=" .. lowest ..")" );
 
-    --logger:debug ( "setBits: reg=" .. tohex ( register ) .. " highest=" .. highest .. " lowest=" .. lowest .. " value=" .. tohex ( value ) )
+    --logger:debug ( "setBits: register=" .. tohex ( register ) .. " highest=" .. highest .. " lowest=" .. lowest .. " value=" .. tohex ( value ) );
 
-    local old = M.readByte ( reg );
+    local old = M.readByte ( register );
 
     local mask = 0;
     for i = 0, highest - lowest do
@@ -99,14 +131,14 @@ function M.setBits ( reg, highest, lowest, value )
 
     local new = bit.bor ( bit.band ( old, bit.bxor ( bit.lshift ( mask, lowest ), 0xFF ) ), bit.lshift ( bit.band ( value, mask ), lowest ) );
 
-    local n = M.writeByte ( reg, new );
-    --logger:debug ( "setBits: n=" .. n )
+    local n = M.writeByte ( register, new );
+    --logger:debug ( "setBits: n=" .. n );
 
 end
 
 function M.readWord ( highByteReg, lowByteReg )
 
-    --logger:debug ( "setWord: highByteReg=" .. tohex ( highByteReg ) .. " lowByteReg=" .. tohex ( lowByteReg ) .. " threshold=" .. threshold )
+    --logger:debug ( "readWord: highByteReg=" .. tohex ( highByteReg ) .. " lowByteReg=" .. tohex ( lowByteReg ) );
 
     local highValue = M.readByte ( highByteReg );
     local lowValue = M.readByte ( lowByteReg );
@@ -117,22 +149,48 @@ end
 
 function M.writeWord ( highByteReg, lowByteReg, word )
 
-    --logger:debug ( "setWord: highByteReg=" .. tohex ( highByteReg ) .. " lowByteReg=" .. tohex ( lowByteReg ) .. " threshold=" .. threshold )
+    --logger:debug ( "writeWord: highByteReg=" .. tohex ( highByteReg ) .. " lowByteReg=" .. tohex ( lowByteReg ) .. " threshold=" .. threshold );
 
     local highValue = bit.rshift ( bit.band ( word, 0xFF00 ), 8 );
     local lowValue = bit.band ( word, 0x00FF );
 
     local n1 = M.writeByte ( highByteReg, highValue );
     local n2 = M.writeByte ( lowByteReg, lowValue );
-    --logger:debug ( "setWord: n1=" .. n1 .. " n2=" .. n2 )
+    --logger:debug ( "writeWord: n1=" .. n1 .. " n2=" .. n2 );
 
 end
 
-function M.isBit ( reg, pos )
+function M.readWordLSB ( register )
 
-    --logger:debug ( "isBit: reg=" .. tohex ( reg ) .. " pos=" .. pos )
+    assert ( register, "register is undefined" );
 
-    return bit.isset ( M.readByte ( reg ), pos )
+    logger:debug ( "readWordLSB: register=" .. tohex ( register ) );
+
+    local data = M.readBytes ( register, 2 ); -- data is a string of bytes
+
+    return 256 * string.byte ( data, 2 ) + string.byte ( data, 1 );
+
+end
+
+function M.writeWordLSB ( register, word )
+
+    assert ( register, "register is undefined" );
+    assert ( word, "word is undefined" );
+
+    logger:debug ( "writeWordLSB: Register=" .. tohex ( register ) .. " word=" .. tohex ( word, 4 ) );
+
+    local highValue = bit.rshift ( bit.band ( word, 0xFF00 ), 8 );
+    local lowValue = bit.band ( word, 0x00FF );
+
+    M.writeBytes ( register, { lowValue, highValue } );
+
+end
+
+function M.isBit ( register, pos )
+
+    --logger:debug ( "isBit: register=" .. tohex ( register ) .. " pos=" .. pos );
+
+    return bit.isset ( M.readByte ( register ), pos )
 
 end
 
@@ -205,7 +263,7 @@ end
 -------------------------------------------------------------------------------
 -- public functions
 
-function M.init ( address, sda, scl, defaults )
+function M.init ( address, sda, scl, defaults, mode )
 
     --assert ( address, "address is undefined" );
     --assert ( sda, "sda is undefined" );
@@ -213,9 +271,10 @@ function M.init ( address, sda, scl, defaults )
     --assert ( scl, "scl is undefined" );
     --assert ( type ( scl ) == "number", "scl isnt number" );
     --assert ( defaults == nil, "defaults is not nil" );
-    --assert ( verbose == nil or type ( verbose ) == "boolean", "verbose isnt boolean" );
 
-    logger:debug ( "init: sda=" .. sda .. " scl=" .. scl  )
+    if ( not mode ) then mode = "byte" end
+
+    logger:debug ( "init: sda=" .. sda .. " scl=" .. scl .. " mode=" .. mode )
 
     deviceAdress = address;
 
@@ -224,7 +283,11 @@ function M.init ( address, sda, scl, defaults )
 
     if ( defaults and type ( defaults ) == "table" ) then
         for register, value in pairs ( defaults ) do
-            M.writeByte ( register, value );
+            if ( mode == "byte" ) then
+                M.writeByte ( register, value );
+            elseif ( mode == "wordlsb" ) then
+                M.writeWordLSB ( register, value );
+            end
         end
     end
 
